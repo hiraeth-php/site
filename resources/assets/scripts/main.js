26c2d0d75dc857bc5afcc928076fff7b01f530d4
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-11-18 21:46:08 -0800

    copy fixups

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -24,6 +24,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         copy.clpb = document.createElement('textarea');
 
         copy.classList.add('copy');
+        copy.clpb.setAttribute('readonly', 'true');
         copy.addEventListener('click', function(event) {
             event.target.clpb.value = event.target.closest('pre').querySelector('code').innerText;
 
@@ -31,11 +32,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
             document.execCommand('copy');
         });
 
-        copy.innerHTML          = '⎘';
-        copy.clpb.style.display = 'block';
-        copy.clpb.style.border  = 'none';
-        copy.clpb.style.height  = '0';
-        copy.clpb.style.width   = '0';
+        copy.innerHTML             = '⎘';
+        copy.clpb.style.background = 'transparent';
+        copy.clpb.style.overflow   = 'hidden';
+        copy.clpb.style.display    = 'block';
+        copy.clpb.style.outline    = 'none';
+        copy.clpb.style.resize     = 'none';
+        copy.clpb.style.border     = 'none';
+        copy.clpb.style.height     = '0px';
+        copy.clpb.style.width      = '0px';
+        copy.clpb.style.font       = '0px';
 
         copy.append(copy.clpb);
         node.prepend(copy);
